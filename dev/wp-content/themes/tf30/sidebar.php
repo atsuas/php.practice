85ac63ccd2ced80553eda8a898335b2688ab6bf4
as <>
2020-10-27 11:35:26 +0900

    プロフィールと検索ボックスの作成

--- dev/wp-content/themes/tf30/sidebar.php
+++ dev/wp-content/themes/tf30/sidebar.php
@@ -1,9 +1,34 @@
 <!-- secondary -->
 <aside id="secondary">
-    
-<?php if (is_active_sidebar( 'sidebar' ) ) : ?>
-<?php dynamic_sidebar( 'sidebar' ); ?>
 
-<?php endif; ?>
-</aside><!-- secondary -->
+<!-- widget_custom_html -->
+<div class="widget widget_text widget_custom_html">
+    <div class="widget-title">プロフィール</div>
 
+        <div class="wprofile">
+            <div class="wprofile-img"><img src="<?php echo get_template_directory_uri(); ?>/img/profile.png" alt=""></div>
+                <div class="wprofile-content">
+                    <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                </div>
+            <!-- /wprofile-content -->
+                <nav class="wprofile-sns">
+                    <div class="wprofile-sns-item m_twitter"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-twitter"></i></a></div>
+                    <div class="wprofile-sns-item m_facebook"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-facebook-f"></i></a></div>
+                    <div class="wprofile-sns-item m_instagram"><a href="" rel="noopener noreferrer" target="_blank"><i class="fab fa-instagram"></i></a></div>
+                </nav>
+        </div><!-- /wprofile -->
+</div><!-- /widget_custom_html -->
+
+<!-- widget_search -->
+<div class="widget widget_search">
+    <div class="widget-title">検索</div>
+    <!-- search-form -->
+    <form method="get" class="search-form" action="<?php echo home_url('/'); ?>">
+        <input type="search" class="search-field" value="" placeholder="キーワード" name="s" id="s">
+        <button type="submit" class="search-submit"><i class="fas fa-search"></i></button>
+    </form><!-- /search-form -->
+</div><!-- /widget_search -->
+
+<!-- <?php get_search_form(); ?> でもデフォルトの検索フォームを表示できる -->
+
+</aside><!-- /secondary -->
