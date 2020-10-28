50f5f1607bb6da8f6877f6c065df247c701840d8
as <>
2020-10-28 20:17:27 +0900

    sidebarの作成

--- dev/wp-content/themes/tf30/sidebar.php
+++ dev/wp-content/themes/tf30/sidebar.php
@@ -31,4 +31,114 @@
 
 <!-- <?php get_search_form(); ?> でもデフォルトの検索フォームを表示できる -->
 
+
+<!-- widget_popular -->
+<div class="widget widget_popular">
+    <div class="widget-title">人気記事</div>
+
+        <div class="wpost-items m_ranking">
+            <?php
+            // get_post_viewsで適宜アクセス数を確認
+            // $counter = get_post_views();
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 5,
+                'meta_key' => 'view_counter',
+                'orderby' => 'meta_value_num',
+                'order' => 'DESC',
+            );
+            $popular_posts = get_posts( $args );
+            foreach($popular_posts as $post): setup_postdata( $post );
+            ?>
+
+        <!-- wpost-item -->
+        <a class="wpost-item" href="<?php the_permalink(); ?>">
+            <div class="wpost-item-img">
+            <?php
+            if (has_post_thumbnail() ) {
+                // アイキャッチ画像が設定されてれば中サイズで表示
+                the_post_thumbnail('medium');
+            } else {
+                // なければnoimage画像をデフォルトで表示
+                echo '<img src="' . esc_url(get_template_directory_uri()) . '/img/noimg.png" alt="">';
+            }
+            ?>
+            </div>
+            <div class="wpost-item-body">
+                <div class="wpost-item-title"><?php the_title(); ?></div>
+            </div><!-- /wpost-item-body -->
+        </a><!-- /wpost-item -->
+
+        <?php
+        endforeach; wp_reset_postdata();
+        ?>
+
+    </div><!-- /wpost-items -->
+</div><!-- /widget_popular -->
+
+
+<!-- widget_recent -->
+<div class="widget widget_recent">
+    <div class="widget-title">新着記事</div>
+
+        <div class="wpost-items">
+            <?php
+                $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 5,
+                    'orderby' => 'date',
+                    'order' => 'DESC',
+                );
+                    $new_posts = get_posts($args);
+                    foreach($new_posts as $post): setup_postdata( $post );
+            ?>
+
+            <!-- wpost-item -->
+            <a class="wpost-item" href="<?php the_permalink(); ?>">
+                <div class="wpost-item-img">
+                    <?php
+                        if (has_post_thumbnail() ) {
+                        // アイキャッチ画像が設定されてれば中サイズで表示
+                        the_post_thumbnail('medium');
+                    } else {
+                        // なければnoimage画像をデフォルトで表示
+                        echo '<img src="' . esc_url(get_template_directory_uri()) . '/img/noimg.png" alt="">';
+                    }
+                    ?>
+                </div>
+                <div class="wpost-item-body">
+                    <div class="wpost-item-title"><?php the_title(); ?></div>
+                </div><!-- /wpost-item-body -->
+            </a><!-- /wpost-item -->
+
+                    <?php endforeach; wp_reset_postdata(); ?>
+
+        </div><!-- /wpost-items -->
+</div><!-- /widget_recent -->
+
+
+<!-- widget_archive -->
+<div class="widget widget_archive">
+    <div class="widget-title">アーカイブ</div>
+    <ul>
+        <?php
+        //初期値なので$argsは書かなくてもOK
+        $args = array(
+            'type' => 'monthly',
+            'limit' => '',
+            'format' => 'html',
+            'before' => '',
+            'after' => '',
+            'show_post_count' => false,
+            'echo' => 1,
+            'order' => 'DESC',
+            'post_type' => 'post'
+        );
+        wp_get_archives( $args );
+        ?>
+    </ul>
+</div><!-- /widget_archive -->
+
+
+
 </aside><!-- /secondary -->
